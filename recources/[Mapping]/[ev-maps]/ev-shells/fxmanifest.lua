356853f26ee7192cbdf2b5629ed8004aacdef8ca
SoulSp34r <davenbancroft@live.co.uk>
2022-11-20 04:21:45 -0800

    Mapping 2

---
+++ recources/[Mapping]/[ev-maps]/ev-shells/fxmanifest.lua
@@ -0,0 +1,63 @@
+fx_version "cerulean"
+games {"gta5"}
+
+this_is_a_map 'yes'
+
+ 
+files {
+	'playerhouse_hotel/playerhouse_hotel.ytyp',
+	'stream/playerhouse_hotel/playerhouse_hotel.ytyp',
+	'stream/playerhouse_hotel/playerhouse_hote2.ytyp',
+	'stream/playerhouse_tier3/playerhouse_tier3.ytyp',
+	'stream/playerhouse_appartment_motel/playerhouse_appartment_motel.ytyp',
+	'stream/micheal_shell/micheal_shell.ytyp',
+	'stream/trevors_shell/trevors_shell.ytyp',
+	'stream/gunshop_shell/gunshop_shell.ytyp',
+	'stream/traphouse_shell/traphouse_shell.ytyp',
+	'stream/appartment/appartment.ytyp',
+	'stream/caravan_shell/caravan.ytyp',
+	'stream/frankelientje/frankelientje.ytyp',
+	'stream/tante_shell/tante.ytyp',
+	'stream/methlab_shell/methlab_shell.ytyp',
+	'stream/warehouse_shell/warehouse_shell.ytyp',
+	'shellprops.ytyp',
+
+    'shellpropsv9.ytyp',
+    'shellprops.ytyp',
+    'shellpropv2s.ytyp',
+    'shellpropsv5.ytyp',
+    'shellpropsv10.ytyp',
+    'shellpropsv4.ytyp',
+    'v_int_shop.ytyp',
+    'playerhouse_hotel/playerhouse_hotel.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_20.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_hotel/playerhouse_hotel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier1/playerhouse_tier1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier1/playerhouse_tier2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_tier3/playerhouse_tier3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/playerhouse_appartment_motel/playerhouse_appartment_motel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/micheal_shell/micheal_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/trevors_shell/trevors_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gunshop_shell/gunshop_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/traphouse_shell/traphouse_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/appartment/appartment.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/caravan_shell/caravan.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/frankelientje/frankelientje.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/tante_shell/tante.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/methlab_shell/methlab_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/warehouse_shell/warehouse_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
+
+
+
+
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv9.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv5.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv10.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_20.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_shop.ytyp'
